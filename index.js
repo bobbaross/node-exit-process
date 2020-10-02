59b4d3cf04e1ac4f49479a6680019c6a770c8acf
Bobba Ross <67179348+bobbaross@users.noreply.github.com>
2020-10-02 10:32:38 -0400

    I think a log would be useful.
    I think it would be beneficial to have another function in this, as it is very difficult without a package.

--- index.js
+++ index.js
@@ -5,3 +5,11 @@ module.exports = function(code) {
     if (!code) code = 0
     process.exit(code)
 }
+exports.log = (log) => {
+    if(!log) {
+        console.log("where is the log? am I dumb?")
+        process.exit(-1)
+    } else {
+        console.log(log)
+    }
+}
